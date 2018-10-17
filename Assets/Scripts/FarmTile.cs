7f3e5597fc3396acbaf44a3ed2ec3c0489363813
lachiesainsbury <lachies.spare@gmail.com>
2018-10-17 17:31:05 +1100

    Planted crops can now grow

---
+++ Assets/Scripts/FarmTile.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FarmTile {
+
+    private Vector3Int coordinates;
+    private Food crop;
+    private Tilemap tilemap;
+    private int growthStage;
+
+    public FarmTile(Vector3Int coordinates) {
+        crop = null;
+        this.coordinates = coordinates;
+    }
+
+    public void PlantCrop(Food crop, Tile tile, Tilemap tilemap) {
+        this.crop = crop;
+        this.tilemap = tilemap;
+
+        tilemap.SetTile(coordinates, tile);
+        growthStage = 1;
+    }
+
+    public void GrowCrop() {
+        if (growthStage < 4) {
+            Tile tile = GetNextGrowthTile();
+
+            if (tile != null) {
+                tilemap.SetTile(coordinates, tile);
+                growthStage++;
+            }
+        } else {
+            Debug.Log("Crop is fully grown");
+        }
+    }
+
+    public void HarvestCrop() {
+
+    }
+
+    private Tile GetNextGrowthTile() {
+        switch (growthStage) {
+            case 1:
+                return Resources.Load<Tile>("Foods/Plants/" + crop.growthStageTwoTiles[0]);
+
+            case 2:
+                return Resources.Load<Tile>("Foods/Plants/" + crop.growthStageThreeTiles[0]);
+
+            case 3:
+                return Resources.Load<Tile>("Foods/Plants/" + crop.growthStageFourTiles[0]);
+        }
+
+        return null;
+    }
+
+    public Vector3Int GetCoordinates() {
+        return coordinates;
+    }
+
+    public bool HasCrop() {
+        if (crop == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
--- Assets/Scripts/Triggers/FarmTrigger.cs
+++ Assets/Scripts/Triggers/FarmTrigger.cs
@@ -7,65 +7,61 @@ public class FarmTrigger : MonoBehaviour {
     [SerializeField]
     private GameObject inventory;
     [SerializeField]
-    private Tilemap tileDecoration1, object1, object2, foreground;
-    [SerializeField]
-    private Sprite farmland;
+    private Tilemap object1, foreground;
 
     private ActionButton actionButton;
     private GameObject player;
     
     private bool playerWithinZone;
 
+    private FarmTile[] farmTiles;
+
 	// Use this for initialization
 	void Start () {
         actionButton = GameObject.FindGameObjectWithTag("ActionButton").GetComponent<ActionButton>();
         player = GameObject.FindGameObjectWithTag("Player");
+
+        initializeFarmTiles();
+    }
+
+    private void initializeFarmTiles() {
+        farmTiles = new FarmTile[5];
+
+        int xCoordinate = 14;
+
+        for (int i=0; i < farmTiles.Length; i++) {
+            FarmTile newTile = new FarmTile(new Vector3Int(xCoordinate, -8, 0));
+            farmTiles[i] = newTile;
+
+            xCoordinate++;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (playerWithinZone && actionButton.GetClicked()) {
-            // Calculate the players direction and get the coordinates of the cell one space in front of them
-            Vector3Int playerDirection = player.GetComponent<Player>().GetPlayerDirection();
-            Vector3Int targetCell = tileDecoration1.WorldToCell(player.transform.position + playerDirection);
+            /* PLANT CROPS */
+            for (int i=0; i < farmTiles.Length; i++) {
+                if (!farmTiles[i].HasCrop()) {
+                    // Plant crop if player has seeds
+                    Food food = GetSeedsFromInventory();
 
-            if (IsValidFarmland(-8, targetCell, object1)) {
-                Food food = GetSeedsFromInventory();
+                    if (food != null) {
+                        PlantSeeds(farmTiles[i], food, object1);
+                    } else {
+                        Debug.Log("No seeds in inventory.");
+                    }
 
-                if (food != null) {
-                    PlantSeeds(food, targetCell, object1);
-                } else {
-                    Debug.Log("No seeds in inventory.");
-                }
-
-            } else if (IsValidFarmland(-9, targetCell, object2)) {
-                Food food = GetSeedsFromInventory();
-
-                if (food != null) {
-                    PlantSeeds(food, targetCell, object2);
-                } else {
-                    Debug.Log("No seeds in inventory.");
+                    return;
                 }
             }
-
-            // If any fully grown crops in zone THEN harvest
-
-        }
-	}
-
-    private bool IsValidFarmland(int y, Vector3Int targetCell, Tilemap tilemap) {
-        // Check if the cell is farm land
-        if (farmland.Equals(tileDecoration1.GetSprite(targetCell))) {
-            // Check if the cell is on a valid growing row
-            if (targetCell.y == y) {
-                // Check if the cell does not currently contain a crop
-                if (tilemap.GetTile(targetCell) == null) {
-                    return true;
+            /* HARVEST CROPS */
+            for (int i=0; i < farmTiles.Length; i++) {
+                if (farmTiles[i].HasCrop()) {
+                    farmTiles[i].HarvestCrop();
                 }
             }
         }
-
-        return false;
     }
 
     private Food GetSeedsFromInventory() {
@@ -86,10 +82,18 @@ public class FarmTrigger : MonoBehaviour {
         return null;
     }
 
-    private void PlantSeeds(Food food, Vector3Int targetCell, Tilemap tilemap) {
+    private void PlantSeeds(FarmTile farmTile, Food food, Tilemap tilemap) {
         Tile tile = Resources.Load<Tile>("Foods/Plants/" + food.growthStageOneTile);
 
-        tilemap.SetTile(targetCell, tile);
+        farmTile.PlantCrop(food, tile, object1);
+    }
+
+    public void GrowCrops() {
+        for (int i=0; i < farmTiles.Length; i++) {
+            if (farmTiles[i].HasCrop()) {
+                farmTiles[i].GrowCrop();
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
