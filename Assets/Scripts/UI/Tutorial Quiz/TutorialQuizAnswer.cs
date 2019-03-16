40f63b241bcb4b397c0bd9c552d0708d5d0fff0d
lachiesainsbury <lachies.spare@gmail.com>
2019-03-16 15:07:04 +1100

    * Finished tutorial quest

---
+++ Assets/Scripts/UI/Tutorial Quiz/TutorialQuizAnswer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialQuizAnswer : MonoBehaviour {
+
+    [SerializeField]
+    [TextArea]
+    private string feedback;
+
+    [SerializeField]
+    private GameObject nextFrame;
+
+    public void submitAnswer() {
+        transform.parent.parent.GetComponent<UIWindow>().Navigate(nextFrame);
+
+        nextFrame.GetComponentInChildren<Text>().text = feedback;
+    }
+}
